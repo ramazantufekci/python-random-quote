e1828db72e5e68bd3edacd36f1ff088190bd395c
ramazantufekci <ramazan.tufekci@outlook.com>
2019-08-19 08:47:46 +0300

    Random quote bot is working

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,13 @@
+import random
+
 def derya():
-   print("Keep it logically awesome.")
-
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
-
-  #print(quotes)
+   #print("Keep it logically awesome.")
+	f = open("quotes.txt")
+	quotes = f.readlines()
+	f.close()
+	last = 13
+	rnd = random.randint(0,last)
+	print(quotes[rnd])
 
 if __name__== "__main__":
   derya()
